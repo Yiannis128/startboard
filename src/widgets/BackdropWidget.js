de3e3513885ff5521ce04baa3281ea9fd66703b0
Yiannis Charalambous <yiannis128@hotmail.com>
2026-01-08 00:28:54 +0200

    feat: add additional backdrops

--- src/widgets/BackdropWidget.js
+++ src/widgets/BackdropWidget.js
@@ -140,11 +140,14 @@ class BackdropWidget extends StartWidget {
     ];
 
     const fitImages = [
-      'backdrop/G9w1XUpWoAAomGc.jpeg',
-      'backdrop/G9w1XUqXwAAP4CB.jpeg',
-      'backdrop/G9w1XUrXAAEKoX4.jpeg',
-      'backdrop/G9w1XUvXUAAQT1B.jpeg',
-      'backdrop/pexels-photo-449011.jpeg'
+     'backdrop/pexels-photo-449011.jpeg',
+     'backdrop/flower-garden-blue-sky-hokkaido-japan-60628.jpeg',
+     'backdrop/ssha-kolorado-gory-plato-ultra-hd-panorama-4k-5k.jpg'
+     'backdrop/G9w1XUpWoAAomGc.jpeg',
+     'backdrop/G9w1XUqXwAAP4CB.jpeg',
+     'backdrop/G9w1XUrXAAEKoX4.jpeg',
+     'backdrop/G9w1XUvXUAAQT1B.jpeg',
+ 
     ];
 
     // Generate background color option buttons
